8834aa0f1a9acd2b8a6dc266a821792deac8a2c5
PrettyBoyCosmo <cosmodiumcs@gmail.com>
2024-06-30 00:15:16 -0400

    extended display support for polybar

--- .scripts/polybar.sh
+++ .scripts/polybar.sh
@@ -8,7 +8,11 @@ killall -q polybar
 # launch
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar --config=$HOME/.config/polybar/config.ini eDP-1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=$HOME/.config/polybar/config.ini DP-1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=$HOME/.config/polybar/config.ini DP-2 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=$HOME/.config/polybar/config.ini DP-3 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=$HOME/.config/polybar/config.ini DP-4 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar --config=$HOME/.config/polybar/config.ini HDMI-1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar --config=$HOME/.config/polybar/config.ini DP-1-6 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar --config=$HOME/.config/polybar/config.ini DP-1-5 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar --config=/home/bluecosmo/.config/polybar/config.ini DP-1-1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=$HOME/.config/polybar/config.ini HDMI-2 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=$HOME/.config/polybar/config.ini HDMI-3 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=$HOME/.config/polybar/config.ini HDMI-4 2>&1 | tee -a /tmp/polybar1.log & disown
