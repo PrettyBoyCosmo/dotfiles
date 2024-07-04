6c073873c617c3e3400a99d91ec34f9b040a4971
PrettyBoyCosmo <cosmodiumcs@gmail.com>
2024-07-04 17:29:17 -0400

    kali install

---
+++ .scripts/kali-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# kali linux installer
+# created by : bluecosmo
+
+# system updates
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
+ 
+# packages
+sudo apt-get install i3 xonsh git feh lxappearance i3blocks gcc-mingw-w64-x86-64 htop gobuster ffmpeg openjdk-17-jdk hexedit docker.io docker-compose stow fzf ripgrep tmux zoxide kitty exa gcc-multilib ranger spice-vdagent -y
+
+# qemu tools
+systemctl enable spice-vdagentd.service
+
+# hacking
+sudo adduser $USER wireshark
+sudo chmod +x /usr/bin/dumpcap
+wget https://github.com/Giotino/stegsolve/releases/download/v.1.5/StegSolve-1.5-alpha1.jar
+
+# dotfiles
+cd $HOME
+git clone https://github.com/PrettyBoyCosmo/dotfiles
+cd dotfiles
+stow . --adopt
+cd $HOME
--- .scripts/mint-install.sh
+++ .scripts/mint-install.sh
@@ -1,5 +1,6 @@
-# debian installer
-# created by : cosmo
+#!/bin/bash
+# linux mint installer
+# created by : bluecosmo
 
 # system updates
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
@@ -9,9 +10,11 @@ curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscale up
 
 # packages
-sudo apt-get install neofetch spotify-client vlc i3 flameshot xonsh git feh lxappearance go-md2man i3blocks mate-media gcc-mingw-w64-x86-64 htop wireshark brightnessctl nmap playerctl hashcat hydra gobuster dirb btop ffmpeg openjdk-17-jdk proxychains4 hexedit steghide docker.io docker-compose device-tree-compiler stow fzf ripgrep tmux zoxide kitty exa gcc-multilib
+sudo apt-get install spotify-client i3 flameshot xonsh git feh lxappearance i3blocks mate-media gcc-mingw-w64-x86-64 htop wireshark brightnessctl nmap playerctl hashcat hydra gobuster dirb btop ffmpeg openjdk-17-jdk proxychains4 hexedit docker.io docker-compose stow fzf ripgrep tmux zoxide kitty exa gcc-multilib ranger -y
 
-# flatpaks
+# removed packages : go-md2man device-tree-compiler
+
+# flatpaks (built into mint)
 flatpak install flathub md.obsidian.Obsidian com.brave.Browser com.obsproject.Studio org.kde.kdenlive com.discordapp.Discord org.gimp.GIMP im.riot.Riot com.obsproject.Studio.Plugin.waveform com.github.micahflee.torbrowser-launcher net.werwolv.ImHex
 
 # add flatpaks to dmenu
@@ -26,19 +29,26 @@ sudo ln -s /var/lib/flatpak/exports/bin/ com.github.micahflee.torbrowser-launche
 sudo ln -s /var/lib/flatpak/exports/bin/net.werwolv.ImHex /usr/bin/imhex
 
 # powershell
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.3.3/powershell_7.3.3-1.deb_amd64.deb
-sudo dpkg -i powershell_7.3.3-1.deb_amd64.deb
-rm -rf powershell_7.3.3-1.deb_amd64.deb
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.3/powershell_7.4.3-1.deb_amd64.deb
+sudo dpkg -i powershell_7.4.3-1.deb_amd64.deb
+rm -rf powershell_7.4.3-1.deb_amd64.deb
 sudo ln -s /bin/pwsh /bin/powershell
 
-# shodan
+# hacking
 pip install -U --user shodan
 sudo adduser $USER wireshark
 sudo chmod +x /usr/bin/dumpcap
 
+# dotfiles
+cd $HOME
+git clone https://github.com/PrettyBoyCosmo/dotfiles
+cd dotfiles
+stow . --adopt
+cd $HOME
+
 # neovim pluginstall
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       # https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # qemu virtualization
 sudo apt install qemu-kvm qemu-system qemu-utils python3 python3-pip libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
