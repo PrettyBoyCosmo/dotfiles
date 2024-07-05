62e238edf655a698a1b13b0e618c2ab2479166e3
PrettyBoyCosmo <cosmodiumcs@gmail.com>
2024-07-05 16:48:24 -0400

    idek atp

--- .scripts/arch-install.sh
+++ .scripts/arch-install.sh
@@ -11,18 +11,25 @@ chmod +x strap.sh
 sudo ./strap.sh
 
 # system updates
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 # install packages
-sudo pacman -S neovim kitty firefox tmux p7zip git python3 python-pip python-pipx python-pwntools python-netifaces net-tools lua zoxide i3-wm i3blocks i3lock polybar flatpak xorg-xrandr arandr stow fzf flameshot feh lxappearance mate-media mingw-w64-gcc htop wireshark-cli nmap hashcat hydra gobuster dirb btop ffmpeg jdk21-openjdk proxychains-ng hexedit docker docker-compose stow fzf ripgrep exa gcc-multilib gdb gcc nasm xclip ranger rofi spice-vdagent lua -y
-# removed packages : brightnessctl playerctl 
+sudo pacman -S xonsh python-pygments python-prompt_toolkit python-setproctitle python-netifaces python-pwntools neovim kitty firefox tmux p7zip git python3 python-pip python-pipx python-pwntools python-netifaces net-tools lua zoxide i3-wm i3blocks i3lock polybar flatpak xorg-xrandr arandr stow fzf flameshot feh lxappearance mate-media mingw-w64-gcc htop wireshark-cli nmap hashcat hydra gobuster dirb btop ffmpeg jdk21-openjdk proxychains-ng hexedit docker docker-compose stow fzf ripgrep exa gcc-multilib gdb gcc nasm xclip ranger rofi spice-vdagent lua --noconfirm
+# removed packages (arch) : brightnessctl playerctl 
 
 # yay
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 cd $HOME
-yay -Ss picom-git ttf-font-awesome-5 nerd-fonts-git xonsh-git xontrib-vox-git
+yay -S picom-git ttf-font-awesome-5
+# removed packages (aur) : nerd-fonts-git
+
+# xonsh
+# TODO: test this
+pipx install xonsh[full] xontrib-vox --include-deps --force
+pipx inject xonsh xontrib-vox
+yay -S xontrib-vox-git --noconfirm
 
 # clone dotfiles
 git clone https://github.com/PrettyBoyCosmo/dotfiles
@@ -31,15 +38,7 @@ cd .dotfiles
 stow . --adopt
 cd $HOME
 
-# xonsh
-# TODO: test this
-# TARGET_DIR=$HOME/.local/xonsh-env PYTHON_VER=3.11 XONSH_VER='xonsh[full]>=0.17.0' \
-#  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/anki-code/xonsh-install/main/mamba-install-xonsh.sh)"
-# xonsh -c "xpip install -U 'xonsh[full]'"
-pipx install xonsh[full]
-xonsh -c "xpip install xontrib-vox pwn netifaces"
-
-rm -rf arch-install.sh strap.sh
+rm -rf arch-install.sh strap.sh yay-bin
 
 # styling
 # wget https://github.com/ful1e5/Google_Cursor/releases/download/v2.0.0/GoogleDot-White.tar.gz
