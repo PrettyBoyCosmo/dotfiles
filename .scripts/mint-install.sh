1482905ca34931dde79568458f1d2cf3510c5cf9
PrettyBoyCosmo <cosmodiumcs@gmail.com>
2024-07-05 13:09:05 -0400

    i3 mirrors

--- .scripts/mint-install.sh
+++ .scripts/mint-install.sh
@@ -2,8 +2,13 @@
 # linux mint installer
 # created by : bluecosmo
 
+# i3 mirrors
+/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2024.03.04_all.deb keyring.deb SHA256:f9bb4340b5ce0ded29b7e014ee9ce788006e9bbfe31e96c09b2118ab91fca734
+sudo apt install ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update
+
 # system updates
-sudo add-apt-repository http://debian.sur5r.net/i3/ jammy universe
 sudo apt-get update -y
 sudo apt-get update --fix-missing -y
 sudo apt upgrade -y
